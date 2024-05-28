b9de1333cefb89ec976dacf7687f97611577cf40
Rubix <akshobh.verma@gmail.com>
2024-05-28 18:17:04 +0530

    added form disabling on condition

--- Python/VSCode/Flask/Projects/Todo_List/website/views.py
+++ Python/VSCode/Flask/Projects/Todo_List/website/views.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required, current_user
 
 views = Blueprint("views", __name__)
+
+@views.route("/")
+def home_redirect():
+    return redirect(url_for("views.home"))
 
 @views.route("/home")
 @login_required
