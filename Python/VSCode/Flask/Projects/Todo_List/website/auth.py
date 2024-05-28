bbc1ed77a3fb2d5088c41f98c40a09f2349766b3
Rubix <akshobh.verma@gmail.com>
2024-05-28 13:38:22 +0530

    Improved styling

--- Python/VSCode/Flask/Projects/Todo_List/website/auth.py
+++ Python/VSCode/Flask/Projects/Todo_List/website/auth.py
@@ -15,14 +15,6 @@ def login():
 
         user = Users.query.filter_by(email=email).first()
 
-        user_dict = {
-            "email": user.email,
-            "name": user.name,
-            "age": user.age,
-            "gender": user.gender,
-            "date": user.date
-        }
-
         if user:
             if check_password_hash(user.password, password):
                 login_user(user, remember=True)
@@ -30,13 +22,24 @@ def login():
                 return redirect(url_for("views.home"))
             else:
                 flash("Incorrect password!", category='error')
+                return redirect(url_for("auth.login"))
         else:
             flash("User doesn't exist!", category='error')
+            return redirect(url_for("auth.login"))
+        
+        user_dict = {
+                "email": user.email,
+                "name": user.name,
+                "age": user.age,
+                "gender": user.gender,
+                "date": user.date
+            }
     return render_template("login.html", user_dict=user_dict, user=current_user)
 
 # Sign-Up page backend
 @auth.route("/sign-up", methods=["GET","POST"])
 def sign_up():
+    user_dict = {}
     if request.method == "POST":
         email = request.form.get("email")
         name = request.form.get("name")
@@ -47,24 +50,21 @@ def sign_up():
 
         user = Users.query.filter_by(email=email).first()
 
-        user_dict = {
-            "email": email,
-            "name": name,
-            "age": age,
-            "gender": gender,
-            "date": user.date
-        }
-
         if user:
             flash("User already exists!", category='error')
+            return redirect(url_for("auth.sign_up"))
         elif len(name) < 2:
             flash("Name must be greater than 2 letters!", category='error')
+            return redirect(url_for("auth.sign_up"))
         elif age < 18:
             flash("Age must be greater than 18!", category='error')
+            return redirect(url_for("auth.sign_up"))
         elif gender == "":
             flash("Select a gender!", category='error')
+            return redirect(url_for("auth.sign_up"))
         elif password1 != password2:
             flash("Incorrect password!", category='error')
+            return redirect(url_for("auth.sign_up"))
         else:
             new_user = Users(email=email, name=name, age=age, gender=gender, password=generate_password_hash(password1))
             db.session.add(new_user)
@@ -73,7 +73,14 @@ def sign_up():
             login_user(new_user, remember=True)
             flash("User created successfully", category='success')
             return redirect(url_for("views.home"))
-
+        
+        user_dict = {
+            "email": email,
+            "name": name,
+            "age": age,
+            "gender": gender,
+            "date": user.date
+        }
     return render_template("sign-up.html", user_dict=user_dict, user=current_user)
 
 @auth.route("logout")
