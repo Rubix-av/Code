4814dc9917f1bf5c6c729b93d2190eb0346ca679
Rubix <akshobh.verma@gmail.com>
2024-06-05 12:30:23 +0530

    Changes to online calculator

--- JavaScript/Learning/Practice/script.js
+++ JavaScript/Learning/Practice/script.js
@@ -1,7 +1,17 @@
 const input = document.getElementById("input");
+const symbols = ["(",")","/","+","-","*","=","."];
 
 function addToDisplay(a){
 
+    //
+    if(input.value === "Error"){
+        input.value = "";
+    }
+    else if(symbols.includes(input.value[input.value.length - 1]) && symbols.includes(a)){
+        input.value = input.value.slice(0, -1) + a;
+        return
+    }
+    
     input.value += a;
 }
 
@@ -11,7 +21,12 @@ function clearDisplay(){
 
 function calculate(){
     try{
-        input.value = eval(input.value);
+        if(input.value === "Error" || input.value === ""){
+            input.value = "Error";
+        }
+        else{
+            input.value = eval(input.value);
+        }
     }    
     catch(error){
         input.value = "Error";
