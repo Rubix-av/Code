5aa513af9e3470ed13e0c384902438e9034ce9ea
Rubix <akshobh.verma@gmail.com>
2024-06-08 06:02:19 +0530

    other old commit

---
+++ JavaScript/Learning/Projects/31.Calculator/script.js
@@ -0,0 +1,43 @@
+const input = document.getElementById("input");
+const symbols = ["/","+","-","*","=","."];
+
+function addToDisplay(a){
+    if(input.value === "Error" || input.value === "NaN"){
+        input.value = "";
+    }
+    else if(symbols.includes(input.value[input.value.length - 1]) && symbols.includes(a)){
+        input.value = input.value.slice(0, -1) + a;
+        return
+    }
+    
+    input.value += a;
+}
+
+function clearDisplay(){
+    input.value = "";
+}
+
+function calculate(){
+    try{
+        if(input.value === "Error" || input.value === ""){
+            input.value = "Error";
+        }
+        else{
+            input.value = eval(input.value);
+        }
+    }    
+    catch(error){
+        input.value = "Error";
+    }
+}
+
+function backspace(event){
+    if(event.key === "Backspace"){
+        event.preventDefault();
+        input.value = input.value.slice(0, -1);
+    }
+}
+
+function backspaceBtn(){
+    input.value = input.value.slice(0, -1);
+}
