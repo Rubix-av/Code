b4e7211dae45af7bf867e34b6a041a7a7cd77654
Rubix <akshobh.verma@gmail.com>
2024-08-08 15:42:47 +0530

    Started working on Games.vue

--- JavaScript/FrameWorks/VueJs/Vue2/Projects/Games_Library_App/frontend/frontend/src/components/Games.vue
+++ JavaScript/FrameWorks/VueJs/Vue2/Projects/Games_Library_App/frontend/frontend/src/components/Games.vue
@@ -1,5 +1,49 @@
 <template>
-    <div class="container">
-        <p>Games Library</p>
-    </div>
+	<div class="jumbotron vertical-center">
+		<div class="container">
+
+			<!-- bootswatch CDN -->
+			<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/sketchy/bootstrap.min.css"
+				integrity="sha384-RxqHG2ilm4r6aFRpGmBbGTjsqwfqHOKy1ArsMhHusnRO47jcGqpIQqlQK/kmGy9R" crossorigin="anonymous">
+
+			<div class="row">
+				<div class="col-sm-12">
+					<p>Games Library🕹️</p>
+					<hr><br>
+
+					<!-- Alert Message -->
+					<button type="button" class="btn btn-success btn-sm">Add Game</button>
+					<br><br>
+					<table class="table table-hover">
+						<!-- Table Head -->
+						<thead>
+							<tr>
+								<!-- Table Header Cells -->
+								<th scope="col">Title</th>
+								<th scope="col">Genre</th>
+								<th scope="col">Played?</th>
+								<th scope="col">Actions</th>
+
+							</tr>
+						</thead>
+
+						<!-- Table Body -->
+						<tbody>
+							<tr>
+								<td>DO</td>
+								<td>RE</td>
+								<td>MI</td>
+								<td>
+									<div class="btn-group" role="group">
+										<button type="button" class="btn btn-info btn-sm">Update</button>
+										<button type="button" class="btn btn-danger btn-sm">Delete</button>
+									</div>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
