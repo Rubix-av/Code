1591d6d8ea38bebd8ae5e7ddd8ae507fa061bf1a
Rubix <akshobh.verma@gmail.com>
2024-09-10 16:48:36 +0530

    Project Backend Setup

--- JavaScript/FrameWorks/VueJs/Vue2/Projects/User_Profile_App/app.py
+++ JavaScript/FrameWorks/VueJs/Vue2/Projects/User_Profile_App/app.py
@@ -1,40 +1,32 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_security import Security, SQLAlchemyUserDatastore
-import uuid
+from flask import Flask
+import views
+from extensions import db, security
+from create_initial_data import create_data
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'thisisasecret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+def create_app():
+    app = Flask(__name__)
 
-db = SQLAlchemy(app)
+    app.config['SECRET_KEY'] = 'this_should-be*secret'
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+    app.config['SECURITY_PASSWORD_SALT'] = 'salty-password'
 
-role_users = db.Table('roles_users', 
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('role_id', db.Integer, db.ForeignKey('role.id'))
-)
+    db.init_app(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(255))
-    active = db.Column(db.Boolean)
-    confirmed_at = db.Column(db.DateTime)
-    fs_uniquifier = db.Column(db.String(255), unique=True, nullable=False, default=lambda: str(uuid.uuid4()))
+    with app.app_context():
+        from models import User, Roles
+        from flask_security import SQLAlchemyUserDatastore
 
-class Role(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(40))
-    description = db.Column(db.String(255))
+        user_datastore = SQLAlchemyUserDatastore(db, User, Roles)
 
-user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-security = Security(app, user_datastore)
-app.app_context().push()
+        security.init_app(app, user_datastore)
 
-@app.route('/register')
-def register():
-    return render_template('register.html')
+        db.create_all()
+        create_data(user_datastore)
+
+    views.create_view(app)
+    
+    return app
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
-
---
+++ JavaScript/FrameWorks/VueJs/Vue2/Projects/User_Profile_App/views.py
@@ -0,0 +1,11 @@
+from flask import render_template_string
+
+def create_view(app):
+
+    @app.route('/')
+    def home():
+        return render_template_string(
+            '''
+                <h1>This is homepage</h1>
+            '''
+        )
