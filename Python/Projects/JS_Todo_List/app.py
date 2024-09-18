7b1450524c6afcd6800d3c8766337d5d398c61ef
Rubix <akshobh.verma@gmail.com>
2024-09-18 15:31:38 +0530

    JS todo list webapp

--- Python/Projects/JS_Todo_List/app.py
+++ Python/Projects/JS_Todo_List/app.py
@@ -7,13 +7,13 @@ import resources
 def create_app():
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = 'this_should-be*secret'
+    app.config['SECRET_KEY'] = "should-not-be-exposed"
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
     app.config['SECURITY_PASSWORD_SALT'] = 'salty-password'
 
     # configure token
-    app.config['SECURITY_TOKEN_AUTHENTICATION_TOKEN'] = "Authentication-Token"
-    app.config['SECURITY_TOKEN_MAX_AGE'] = 3600 # Token expires in 1hr
+    app.config['SECURITY_TOKEN_AUTHENTICATION_HEADER'] = 'Authentication-Token'
+    app.config['SECURITY_TOKEN_MAX_AGE'] = 3600 #1hr
     app.config['SECURITY_LOGIN_WITHOUT_CONFIRMATION'] = True
 
     db.init_app(app)
@@ -29,14 +29,14 @@ def create_app():
         db.create_all()
         create_data(user_datastore)
 
-    views.create_view(app, user_datastore)
-    
-    # connect flask to flask_restful
-    # resources.api.init_app(app)
-    
     app.config["WTF_CSRF_CHECK_DEFAULT"] = False
     app.config["SECURITY_CSRF_PROTECT_MECHANISHMS"] = []
     app.config["SECURITY_CSRF_IGNORE_UNAUTH_ENDPOINTS"] = True
+
+    views.create_view(app, user_datastore)
+    
+    # setup api
+    resources.api.init_app(app)
 
     return app
 
