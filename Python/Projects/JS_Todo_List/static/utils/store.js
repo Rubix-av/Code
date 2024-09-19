61ca10617ff71038daf29455191f1dbbd5384dfd
Rubix <akshobh.verma@gmail.com>
2024-09-19 20:58:33 +0530

    JS todo list

--- Python/Projects/JS_Todo_List/static/utils/store.js
+++ Python/Projects/JS_Todo_List/static/utils/store.js
@@ -1,11 +1,11 @@
 const store = new Vuex.Store({
-    state: {
-      origin: window.location.origin,
-      loggedIn: !!sessionStorage.getItem('token'), //sets value true/false based on token's presence
-      token: sessionStorage.getItem('token') || "", //uses token value form sessionStorage if present else ""
-      role: sessionStorage.getItem('role'),
-      email: sessionStorage.getItem('email'),
-      id: sessionStorage.getItem('id')
+	state: {
+    origin: window.location.origin,
+    loggedIn: !!sessionStorage.getItem('token'), //sets value true/false based on token's presence
+    token: sessionStorage.getItem('token') || "", //uses token value form sessionStorage if present else ""
+		role: sessionStorage.getItem('role'),
+		email: sessionStorage.getItem('email'),
+    id: sessionStorage.getItem('id')
     },
     
     mutations: {
