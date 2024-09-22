83b8f81c100c1eefb228965752fccea38e610f57
Rubix <akshobh.verma@gmail.com>
2024-09-22 20:56:27 +0530

    JS_Todo_List/-> Todos can now be deleted and added using on the frontend interface

--- Python/Projects/JS_Todo_List/resources.py
+++ Python/Projects/JS_Todo_List/resources.py
@@ -1,12 +1,13 @@
 from flask import jsonify
 from flask_restful import Resource, Api, reqparse, fields, marshal_with
 from flask_security import auth_required
-from models import Todo
+from models import Todo, User
 from extensions import db
 
 parser = reqparse.RequestParser()
 parser.add_argument('title', type=str, help="Topic should be string", required=True)
 parser.add_argument('desc', type=str, help="Description should be string")
+parser.add_argument('date_created', type=str, help="Date should be string")
 
 api = Api(prefix='/api')
 
@@ -14,7 +15,8 @@ todo_fields = {
     'id': fields.Integer,
     'title': fields.String,
     'desc': fields.String,
-    'user_id': fields.Integer
+    'user_id': fields.Integer,
+    'date_created': fields.DateTime(dt_format='iso8601')
 }
 
 class Todos(Resource):
@@ -45,6 +47,37 @@ class Todos(Resource):
             db.session.rollback()
             return {"message": "creating todo failed", "todo_id": todo.id}, 420
 
-        return {"message": "todo created", "todo_id": todo.id}, 200    
-    
+        return {"message": "todo created", "todo_id": todo.id}, 200
+
+    @auth_required('token')
+    def delete(self, id):
+        todo = Todo.query.get(id)
+
+        if not todo:
+            return {"message": "todo not found"}, 404
+        
+        try:
+            db.session.delete(todo)
+            db.session.commit()
+            return {"message": "todo deleted successfully"}, 200
+        except:
+            db.session.rollback()
+            return {"message": "error deleting todo"}, 500
+
+class UserProfile(Resource):
+    @auth_required('token')
+    def get(self, id):
+        user = User.query.filter_by(id=id).first()
+        if not user:
+            return {"message": "user not found"}, 404
+        
+        user_data = {
+            "id": user.id,
+            "email": user.email,
+            "roles": [role.name for role in user.roles],
+        }
+
+        return user_data, 200
+
 api.add_resource(Todos, '/todos/<int:id>', '/todos')
+api.add_resource(UserProfile, '/users/<int:id>', '/users')
--- Python/Projects/JS_Todo_List/static/pages/Profile.js
+++ Python/Projects/JS_Todo_List/static/pages/Profile.js
@@ -1,39 +1,53 @@
 const Profile = {
   template: `
     <div>
-      <div v-if="allTodos.length === 0">
-        No todos available.
-      </div>
-      <div v-else v-for="todo in allTodos" :key="todo.id">
-        <h3>Title: {{ todo.title }}</h3>
-        <p>Desc: {{ todo.desc }}</p>
-        <p>Id: {{ todo.id }}</p>
-        <p>User Id: {{ todo.user_id }}</p>
+      <div v-for="(value, key, index) in userDetails" :key="index" class="container mt-1">
+        <div class="accordion" :id="index + 'Example'">
+          <div class="accordion-item">
+            <h2 class="accordion-header" :id="'heading' + index">
+              <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                :data-bs-target="'#collapse' + index" aria-expanded="true"
+                :aria-controls="'collapse' + index">
+                <strong>{{ key }}</strong>
+              </button>
+            </h2>
+            <div :id="'collapse' + index" class="accordion-collapse collapse" 
+                  :aria-labelledby="'heading' + index" :data-bs-parent="'#accordion' + index">
+              <div class="accordion-body">
+                {{ value }}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   `,
 
   data() {
     return {
-      allTodos: [],
+      userDetails: {},
     }
   },
 
   async mounted() {
     const origin = this.$store.state.origin;
+    const user_id = sessionStorage.getItem('id');
     const token = sessionStorage.getItem('token');
-    const user_id = Number(sessionStorage.getItem('id'));
 
-    console.log("token: ", token);
-    const res = await fetch(origin + `/api/todos/${user_id}`, {
+    const res = await fetch(origin + `/api/users/${user_id}`, {
       headers: {
-        "Authentication-Token": token
-      },
+        "Authentication-Token": token,
+        "Content-Type": "application/json"
+      }
     });
-    console.log("One")
-    const data = await res.json();
-    console.log("Two")
-    this.allTodos = data;
+    
+    if (res.ok) {
+      console.log("User data fetched successfully");
+      const data = await res.json();
+      this.userDetails = data;
+    } else {
+      console.log("Failed to fetch user data");
+    }
   }
 };
 
