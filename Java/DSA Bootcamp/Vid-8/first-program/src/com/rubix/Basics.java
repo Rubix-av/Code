597b23a4667601d497527190340c0fad42d97892
Rubix <akshobh.verma@gmail.com>
2024-12-11 20:50:09 +0530

    Java DSA Bootcamp

---
+++ Java/DSA Bootcamp/Vid-8/first-program/src/com/rubix/Basics.java
@@ -0,0 +1,24 @@
+package com.rubix;
+
+public class Basics {
+    public static void main(String[] args) {
+//        int a = 10;
+//        if (a == 10){
+//            System.out.println("Hello World");
+//        } else{
+//            System.out.println("Bye World");
+//        }
+
+        // while loop
+//        int count = 1;
+//        while(count!=5) {
+//            System.out.println(count);
+//            count++;
+//        }
+
+        // for loop
+        for(int count = 1; count!=5; count++) {
+            System.out.println(count);
+        }
+    }
+}
