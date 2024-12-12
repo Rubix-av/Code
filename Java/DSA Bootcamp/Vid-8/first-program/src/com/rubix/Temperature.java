d55c1877b17800b93d59a3abe2ce9e46ddcef7c1
Rubix <akshobh.verma@gmail.com>
2024-12-12 21:02:34 +0530

    Java DSA Bootcamp-2

--- Java/DSA Bootcamp/Vid-8/first-program/src/com/rubix/Temperature.java
+++ Java/DSA Bootcamp/Vid-8/first-program/src/com/rubix/Temperature.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Temperature {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Please enter temperature in C: ");
+        System.out.print    ("Please enter temperature in C: ");
         float tempC = in.nextFloat();
 
         float tempF = (tempC * 9/5) + 32;
