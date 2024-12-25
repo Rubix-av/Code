50e5e2b1448ae33da8c5a8bd7e3cd68d2619d595
Rubix <akshobh.verma@gmail.com>
2024-12-25 14:22:00 +0530

    Java DSA Bootcamp Vid-11

---
+++ Java/DSA Bootcamp/Vid-11/functions/src/com/rubix/Greetings.java
@@ -0,0 +1,11 @@
+package com.rubix;
+
+public class Greetings {
+    public static void main(String[] args){
+        greeting();
+    }
+
+    static void greeting() {
+        System.out.println("Hello World");
+    }
+}
---
+++ Java/DSA Bootcamp/Vid-11/functions/src/com/rubix/Scope.java
@@ -0,0 +1,13 @@
+package com.rubix;
+
+public class Scope {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+    }
+
+    static void random() {
+        int num = 67;
+        System.out.println(num);
+    }
+}
