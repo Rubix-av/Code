7715859c61187e7b64ca3fec92e775c48cbf601b
Rubix <akshobh.verma@gmail.com>
2024-12-27 11:23:40 +0530

    Java DSA Bootcamp Vid-12

---
+++ Java/DSA Bootcamp/Vid-12/code/src/com/rubix/Input.java
@@ -0,0 +1,26 @@
+package com.rubix;
+
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int[] arr = new int[5];
+//        arr[0] = 23;
+//        arr[1] = 45;
+//        arr[2] = 233;
+//        arr[3] = 543;
+//        arr[4] = 3;
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
