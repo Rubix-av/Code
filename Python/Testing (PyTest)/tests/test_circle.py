ff4ba9f652feba851a7586e9587701e4c7347ecc
Rubix <akshobh.verma@gmail.com>
2025-10-31 22:10:00 +0530

    Pytest

--- Python/Testing (PyTest)/tests/test_circle.py
+++ Python/Testing (PyTest)/tests/test_circle.py
@@ -10,9 +10,15 @@ class TestCircle:
 
     def teardown_method(self, method):
         print(f"Tearing down {method}")
+        del self.circle
 
     def test_area(self):
         assert self.circle.area() == math.pi * self.circle.radius**2
 
-    def test_two(self):
-        assert True
+    def test_perimeter(self):
+        result = self.circle.perimeter()
+        expected = 2 * math.pi * self.circle.radius
+        assert result == expected
+
+    def test_not_same_area_rectangle(self, my_rectangle):
+        assert self.circle.area() != my_rectangle.area()
